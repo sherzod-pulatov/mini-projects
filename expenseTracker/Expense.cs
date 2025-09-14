193a36aa082bc0310be03d0b0f52d77d719c43de
Pulatov Sherzod <pulat5212@gmail.com>
2025-09-14 18:29:06 +0500

    3rd mini-project uploaded

---
+++ expenseTracker/Expense.cs
@@ -0,0 +1,75 @@
+﻿namespace expenseTracker
+{
+    internal class Expense
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+
+        public Expense(string name, decimal amount, DateTime date, string category)
+        {
+            Name = name;
+            Amount = amount;
+            Date = date;
+            Category = category;
+        }
+
+        public static void AddExpense(List<Expense> expenses)
+        {
+            Expense expense = new Expense("", 0, DateTime.Now, "");
+
+            Console.Write("Name: ");
+            expense.Name = Console.ReadLine()!;
+            Console.Write("Amount: ");
+            expense.Amount = Convert.ToDecimal(Console.ReadLine()!);
+            Console.Write("Category: ");
+            expense.Category = Console.ReadLine()!;
+            Console.Write("Date (mm/dd/yyyy): ");
+            expense.Date = DateTime.Parse(Console.ReadLine()!);
+            expenses.Add(expense);
+        }
+
+        public static void Print(List<Expense> expenses)
+        {
+            foreach (Expense expense in expenses)
+                Console.WriteLine(expense);
+        }
+
+        public static void Filter(List<Expense> expenses, ref List<Expense> filtered)
+        {
+            Console.WriteLine("Filter by Category (Enter 'C')  |  Filter by Date (Enter 'D')");
+            string choice = Console.ReadLine()!;
+            switch (choice.ToLower())
+            {
+                case "c":
+                    Console.Write("Enter Category: ");
+                    string category = Console.ReadLine()!;
+                    filtered = expenses.Where(e => e.Category.ToLower() == category.ToLower()).ToList();
+                    break;
+                case "d":
+                    Console.Write("From (mm/dd/yyyy): ");
+                    var from = DateTime.Parse(Console.ReadLine()!);
+                    Console.Write("To (mm/dd/yyyy): ");
+                    var to = DateTime.Parse(Console.ReadLine()!);
+
+                    if (from > to)
+                    {
+                        Console.WriteLine("Invalid date range !");
+                        break;
+                    }
+
+                    filtered = expenses.Where(e => e.Date >= from && e.Date <= to).OrderBy(d => d.Date).ToList();
+                    break;
+                default:
+                    Console.WriteLine("Choose proper option !");
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}:\t{Amount} ¥;\t{Category}; {Date:d};";
+        }
+    }
+}
---
+++ expenseTracker/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace expenseTracker;
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using static expenseTracker.Expense;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Expense> expenses = new List<Expense>()
+        {
+            new Expense("Groceries", 42, DateTime.Parse("09/07/2025"), "Food"),
+            new Expense("Lunch at uni", 3.44m, DateTime.Today, "Food"),
+            new Expense("Train Ticket", 15.5m, DateTime.Parse("08/01/2025"), "Commute")
+        };
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t\tMY EXPENSE TRACKER\n");
+            Console.Write("A. Add New\tF. Filter By\tT. Total Spent\tQ. Quit\n\n");
+            Print(expenses);
+
+            string choice = Console.ReadLine()!;
+            Console.WriteLine();
+            switch (choice.ToLower())
+            {
+                case "a":
+                    AddExpense(expenses);
+                    break;
+                case "f":
+                    var filtered = new List<Expense>();
+                    Filter(expenses, ref filtered);
+                    Print(filtered);
+                    break;
+                case "t":
+                    Console.WriteLine($"Total money spent: {expenses.Sum(e => e.Amount)} ¥");
+                    break;
+                case "q":
+                    return;
+                default:
+                    Console.WriteLine("Choose proper option !");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
